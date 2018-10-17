6a2f4669fec3a08a816ca7a348e48f0f2f4e9d3c
Satyaban Mahakud <satyaban.mahakud@rediffmail.com>
2018-10-17 16:37:58 +0530

    Added testcase and .project file

---
+++ src/test/java/clinic/programming/training/ApplicationTest.java
@@ -0,0 +1,15 @@
+package clinic.programming.training;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+	private Application app;
+
+	@Before
+	public void setup() {
+		app = new Application();
+	}
+}

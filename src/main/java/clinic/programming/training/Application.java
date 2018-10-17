f9e3aee82e46e9c70d5b0b38e1c81e26ab01d541
Satyaban Mahakud <satyaban.mahakud@rediffmail.com>
2018-10-17 16:10:36 +0530

    Added gitignore file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -5,38 +5,35 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class Application
-{
-    public Application()
-    {
-        System.out.println("Inside Application");
-    }
+public class Application {
+	public Application() {
+		System.out.println("Inside Application");
+	}
 
-    public void greet(){
-        List<String> greetings = new ArrayList<>();
-        greetings.add("Hello");
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
 
-        for (String greeting : greetings) {
-            System.out.println("Greeting: " + greeting);            
-        }
-    }
+		for (String greeting : greetings) {
+			System.out.println("Greeting: " + greeting);
+		}
+	}
 
-    // Returns word count.
-    public int countWords(String strInput){
-        String [] words = StringUtils.split(strInput,' ');
-        return (words == null) ? 0 : words.length;
-    }
+	// Returns word count.
+	public int countWords(String strInput) {
+		String[] words = StringUtils.split(strInput, ' ');
+		return (words == null) ? 0 : words.length;
+	}
 
-    public static void main(String [] args)
-    {
-        System.out.println("Starting Application");
-        Application app = new Application();
-        
-        // Calling greeting function.
-        app.greet();
-       
-        // Calling word count function.
-        String inputString = "This morning i got up very late.";       
-        System.out.println(inputString + " contain " + app.countWords(inputString) + "words.");
-    }
+	public static void main(String[] args) {
+		System.out.println("Starting Application");
+		Application app = new Application();
+
+		// Calling greeting function.
+		app.greet();
+
+		// Calling word count function.
+		String inputString = "This morning i got up very late.";
+		System.out.println(inputString + " contain " + app.countWords(inputString) + "words.");
+	}
 }

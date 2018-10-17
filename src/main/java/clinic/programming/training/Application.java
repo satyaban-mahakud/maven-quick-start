494abc83f3bb260ccb9d324e06227b0dfa81336d
Satyaban Mahakud <satyaban.mahakud@rediffmail.com>
2018-10-17 14:11:42 +0530

    Adding wordcount feature

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,18 +3,13 @@ package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application
 {
     public Application()
     {
         System.out.println("Inside Application");
-    }
-
-    public static void main(String [] args)
-    {
-        System.out.println("Starting Application");
-        Application app = new Application();
-        app.greet();
     }
 
     public void greet(){
@@ -24,5 +19,24 @@ public class Application
         for (String greeting : greetings) {
             System.out.println("Greeting: " + greeting);            
         }
+    }
+
+    // Returns word count.
+    public int countWords(String strInput){
+        String [] words = StringUtils.split(strInput,' ');
+        return (words == null) ? 0 : words.length;
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("Starting Application");
+        Application app = new Application();
+        
+        // Calling greeting function.
+        app.greet();
+       
+        // Calling word count function.
+        String inputString = "This morning i got up very late.";       
+        System.out.println(inputString + " contain " + app.countWords(inputString) + "words.");
     }
 }
